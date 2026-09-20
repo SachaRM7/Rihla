cf260fe401b3c35e630b14dc1794348e2875c4f4
SachaRM7 <sacha4dev@gmail.com>
2026-09-20 08:19:48 +0200

    fix(quran): avoid counting deep links before text view opens

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -256,11 +256,8 @@ export function AppShell() {
     setReciterId(isValidReciter(library.reciterId) ? library.reciterId : DEFAULT_RECITER_ID);
     requestedAyahRef.current = hasValidLink ? linkedAyah : null;
     requestedPositionRef.current = hasValidLink ? linkedTime * 1000 : library.lastPositionMs;
-    if (hasValidLink) {
-      saveReadingProgress(linkedSurah, linkedAyah);
-      window.requestAnimationFrame(() => setActiveView("quran"));
-    }
-  }, [hydrated, library.lastPositionMs, library.lastSurah, library.reciterId, saveReadingProgress]);
+    if (hasValidLink) window.requestAnimationFrame(() => setActiveView("quran"));
+  }, [hydrated, library.lastPositionMs, library.lastSurah, library.reciterId]);
 
   useEffect(() => () => {
     if (shareTimerRef.current !== null) window.clearTimeout(shareTimerRef.current);
